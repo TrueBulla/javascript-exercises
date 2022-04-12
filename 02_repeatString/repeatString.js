a853262eb88d586496016bf9c30c8f2ab07483f7
Matt <schoof.matt@gmail.com>
2022-04-11 20:26:03 -0400

    finished exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(stringToRepeat, numberOfRepeats) {
+    if(numberOfRepeats < 0){
+        return "ERROR";
+    }
+    let string = '';
+    for(let i=0; i<numberOfRepeats; i++){
+        string += stringToRepeat;
+        
+    }
+    return string;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
-
+const reverseString = function(stringToReverse) {
+    let stringIndex = stringToReverse.length - 1;
+    let reversedString = ""
+    while(stringIndex != -1){
+        reversedString += stringToReverse[stringIndex];
+        stringIndex--;
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+//Set stringIndex equal to the length of the string we want to reverse
+//Create an empty string for each letter to go into 
+//while stringIndex is not equal to 0, grab each letter of the string 
+//  (starting from the last one), and add it to the empty string
+//
