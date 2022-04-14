30d8048b3c1bf95ef48446391f0b21d47e65ae26
Matt <schoof.matt@gmail.com>
2022-04-14 11:47:31 -0700

    finished exercise w/ all tests completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,21 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    const newArray = [];
+    array.forEach((item) => {
+        if(!args.includes(item)){
+            newArray.push(item);
+        }
+    });
+    return newArray;
 };
+
+/*
+-------Psuedocode-------
+
+Receive array and the values to remove from the array.
+Interate through the array looking for those numbers 
+If the current array value is NOT one of those numbers, put that value in a newArray.
+*/
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
