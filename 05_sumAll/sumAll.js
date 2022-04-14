b9d0d33d808848f9f7a9336e67ef73d630d3586f
Matt <schoof.matt@gmail.com>
2022-04-14 12:09:20 -0700

    finished exersize w/ all tests passing

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, secondNum) {
+    let total = 0;
+    if((Math.sign(firstNum) === 1 && Math.sign(secondNum) === 1) && (typeof(firstNum) === 'number' && typeof(secondNum) === 'number')){
+        if(firstNum < secondNum){
+            for(firstNum; firstNum<=secondNum; firstNum++){
+                total+=firstNum;
+            }
+        }else{
+            for(firstNum; firstNum>=secondNum; firstNum--){
+                total-=(-firstNum);
+            }
+        }
+        return total;
+    }else{
+        return 'ERROR';
+    }
 };
+/**
+ * PSUEDOCODE
+ * Make sure both your numbers are positive and are actually numbers
+ * Compare the two numbers given to see which one is bigger 
+ * If the first number is bigger, take that number n and add n + 1 to it
+ * If the second number is bigger, take that number n and add n - 1 to it. 
+ */
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
