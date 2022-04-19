bd2a9169aa02101fbd69c4d1e10b63ed64f5839a
Matt <schoof.matt@gmail.com>
2022-04-18 20:25:20 -0400

    finished exercise w/ all tests passing

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+  let newTempCelsius = (tempF - 32) * (5/9);
+  return Math.round(newTempCelsius * 10)/10
 };
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+  let newTempFahren = (tempC * (9/5) + 32)
+  return Math.round(newTempFahren * 10)/10
 };
 
 // Do not edit below this line
