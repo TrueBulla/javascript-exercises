ab75fcc1e7dc370a86ac61738e522246144a63df
matt <matt@matts-MacBook-Air.local>
2022-06-14 22:40:31 -0400

    finished exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, item) => {
+    return total += item;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, item) => {
+    return total * item;
+  }, 0)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let total = a;
+  if(a===0){
+    return total = 1;
+  }
+  for(a; a>=2; a--){
+    total = total * (a-1);
+  }
+  return total
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (word){
+    let filteredArray = word.toLowerCase().replace(/[^a-z]/g, "");
+    return filteredArray.split("").reverse().join("") == filteredArray;
 };
+
+console.log(palindromes('ZZZZ car, a man, a maracaz.'))
 
 // Do not edit below this line
 module.exports = palindromes;
