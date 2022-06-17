cdcafa522f2765700f9ad62bf5cd77cb72d6d048
matt <matt@matts-MacBook-Air.local>
2022-06-17 00:16:24 -0400

    finished exercise. used solution. didn't understand reduce()

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(ageArray) {
+    return ageArray.reduce((oldest, currentOldest) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath)
+        const currentAge = getAge(currentOldest.yearOfBirth, currentOldest.yearOfDeath)
+        return oldestAge < currentAge ? currentOldest : oldest;
+    });
 };
+
+const getAge = (birth, death) => {
+    if(!death) {death = new Date().getFullYear();}
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
